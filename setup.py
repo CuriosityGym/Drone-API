0a3939bd9f4f01233a5b241cd6170ac42827c941
rupin <rupin.chheda@gmail.com>
2018-01-27 15:53:20 +0530

    Deploy

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'droneAPI',
     packages = ['droneAPI'], 
-    version = '0.4',
+    version = '0.4.1',
     description = 'droneAPI is a Python API Wrapper from Pluto Drone and other drones running MultiWii Protocol, over Wifi/ESP8266',
     author = 'CuriosityGym',
     author_email = 'explore@curiositygym.com',
     url = 'https://github.com/CuriosityGym/Drone-API',
-    download_url = 'https://github.com/CuriosityGym/Drone-API/archive/0.4.tar.gz',
+    download_url = 'https://github.com/CuriosityGym/Drone-API/archive/0.4.1.tar.gz',
     keywords = ['multiwii', 'api', 'drone', 'pluto', 'esp8266'], 
     classifiers = [],
     install_requires=[]
